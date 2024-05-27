d1541901ef999fafaa6c83d994f0c288ef507631
FireIsGood <fireisadequate@gmail.com>
2024-05-26 20:01:20 -0700

    fix: change notify title

--- lua/pond-nvim/util.lua
+++ lua/pond-nvim/util.lua
@@ -6,7 +6,7 @@ local M = {}
 ---@param level? "info"|"trace"|"debug"|"warn"|"error"
 function M.notify(msg, level)
   level = level or "info"
-  vim.notify(msg, vim.log.levels[level:upper()], { title = "pond-nvim" })
+  vim.notify(msg, vim.log.levels[level:upper()], { title = "pond.nvim" })
 end
 
 --------------------------------------------------------------------------------
